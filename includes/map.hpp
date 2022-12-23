075115d32d59640270d483de46d1e162e6f3812b
chichoon <chichoon.choi@gmail.com>
2022-12-23 19:23:23 +0900

    chore: map, stack OCF로 초기화만

--- includes/map.hpp
+++ includes/map.hpp
@@ -6,7 +6,22 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 13:14:50 by jiychoi           #+#    #+#             */
-/*   Updated: 2022/12/23 13:14:50 by jiychoi          ###   ########.fr       */
+/*   Updated: 2022/12/23 14:38:55 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MAP_HPP
+# define MAP_HPP
+
+namespace ft {
+    class map {
+        private:
+        public:
+            map();
+            map(const map& instance);
+            ~map();
+            map&  operator=(const map& instance);
+    }
+}
+
+#endif
--- includes/stack.hpp
+++ includes/stack.hpp
@@ -6,7 +6,30 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 13:13:45 by jiychoi           #+#    #+#             */
-/*   Updated: 2022/12/23 13:15:23 by jiychoi          ###   ########.fr       */
+/*   Updated: 2022/12/23 16:35:11 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef STACK_HPP
+# define STACK_HPP
+
+namespace ft {
+    template <typename T>
+    class stack {
+        private:
+            T       *stack_arr;
+        public:
+            stack();
+            ~stack();
+            stack&  operator=(const stack& instance);
+
+            T       top();
+            bool    empty() const;
+            size_t  size();
+
+            void    push(const T& value);
+            void    pop();
+    };
+}
+
+#endif
