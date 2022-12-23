c38c66b157469267f34ddf1103be8c14b6d32c10
chichoon <chichoon.choi@gmail.com>
2022-12-23 16:53:20 +0900

    feat: vector 생성자 소멸자 만듦

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -6,7 +6,29 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 13:14:48 by jiychoi           #+#    #+#             */
-/*   Updated: 2022/12/23 13:14:49 by jiychoi          ###   ########.fr       */
+/*   Updated: 2022/12/23 16:40:59 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+#include <iostream>
+
+namespace ft {
+    template <typename T>
+    class vector {
+        private:
+        public:
+            vector() {
+                std::cout << "vector constructor\n";
+            }
+            vector(const vector& instance);
+            ~vector() {
+                std::cout << "vector destructor\n";
+            }
+            vector&  operator=(const vector& instance);
+    };
+}
+
+#endif
