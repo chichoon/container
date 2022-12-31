7c8da729f7d77d8d689ef8728f0927b4af4fe487
chichoon <chichoon.choi@gmail.com>
2023-01-01 01:39:14 +0900

    feat: vector 생성자들

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 13:14:48 by jiychoi           #+#    #+#             */
-/*   Updated: 2022/12/23 19:30:11 by jiychoi          ###   ########.fr       */
+/*   Updated: 2023/01/01 01:30:47 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,39 +19,52 @@
 namespace ft {
     template <typename T, class Allocator = std::allocator<T> >
     class vector {
-        private:
-            const Allocator&    _alloc;
-            T*                  _array;
-        public:
-            typedef T                                           value_type;
-            typedef Allocator                                   allocator_type;
-            typedef typename allocator_type::reference          reference;
-            typedef typename allocator_type::const_reference    const_reference;
-            typedef typename allocator_type::size_type          size_type;
-            typedef typename allocator_type::difference_type    difference_type;
-            typedef typename allocator_type::pointer            pointer;
-            typedef typename allocator_type::const_pointer      const_pointer;
-            vector(void) {
-                
-            }
-            vector(const allocator_type& alloc = Allocator()): _alloc(alloc) {
-                _array = alloc.allocate(0);
-                std::cout << "vector constructor 2\n";
-            }
-            // vector(size_t count, const T& value, const allocator_type& alloc) {
-            //     std::cout << "vector constructor 3\n";
-            // }
-            // template <class InputIterator>
-            // vector(InputIterator first, InputIterator last, const allocator_type& alloc) {
-            //     std::cout << "vector constructor 4\n";
-            // }
-            // vector(const vector& other) {
-            //     std::cout << "vector copy constructor\n";
-            // }
-            ~vector() {
-                std::cout << "vector destructor\n";
-            }
-            vector&  operator=(const vector& instance);
+    public:
+        typedef T value_type;
+        typedef Allocator allocator_type;
+        typedef typename allocator_type::reference reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::size_type size_type;
+        typedef typename allocator_type::difference_type difference_type;
+        typedef typename allocator_type::pointer pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
+
+    private:
+        allocator_type _alloc;
+        pointer _base_ptr;
+        size_type _size;
+        size_type _capacity;
+
+    public:
+        explicit vector(const allocator_type& alloc = allocator_type()) // 암묵적 생성자 형변환 금지
+            : _alloc(alloc)
+            , _base_ptr(0)
+            , _size(0)
+            , _capacity(0) { }
+
+        explicit vector(size_type count, value_type value, const allocator_type& alloc = allocator_type())
+            : _alloc(alloc)
+            , _size(count)
+            , _capacity(count) {
+            _base_ptr = _alloc.allocate(count);
+            for (size_type i = 0; i < count; i++)
+                _base_ptr[i] = value;
+        }
+
+        template <class Iterator>
+        vector(Iterator first, Iterator last,
+            const allocator_type& alloc = allocator_type())
+            : _alloc(alloc)
+            , _size(last - first + 1)
+            , _capacity(last - first + 1) {
+            _base_ptr = _alloc.allocate(_size);
+        }
+
+        vector(const vector& other) { std::cout << "vector copy constructor\n"; }
+
+        ~vector() { std::cout << "vector destructor\n"; }
+
+        vector& operator=(const vector& instance);
     };
 }
 
