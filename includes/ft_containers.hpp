db14d1d5e12e4b60a2d8f542fc5fd43495a806fd
chichoon <chichoon.choi@gmail.com>
2023-01-01 02:05:37 +0900

    feat: equal 끝 (아마도)

--- includes/ft_containers.hpp
+++ includes/ft_containers.hpp
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 14:49:04 by jiychoi           #+#    #+#             */
-/*   Updated: 2023/01/01 01:56:40 by jiychoi          ###   ########.fr       */
+/*   Updated: 2023/01/01 02:02:59 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,19 @@
 namespace ft {
 template <class InputIt1, class InputIt2>
 bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2) {
+    while (first1 != last1) {
+        if (*first1 != *first2)
+            return false;
+        first1++;
+        first2++;
+    }
+
+    return true;
+}
+
+template <class InputIt1, class InputIt2>
+bool lexicographical_compare(InputIt1 first1, InputIt1 last1,
+    InputIt2 first2, InputIt2 last2) {
 }
 }
 
